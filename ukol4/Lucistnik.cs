9198ff8656498294a8171a49bc02e4dd15ed6d7c
SimonaAda <166747901+SimonaAda@users.noreply.github.com>
2024-04-25 14:15:55 +0200

    opakovani

---
+++ ukol4/Lucistnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ukol4
+{
+    internal class Lucistnik
+    {
+        public int PocetSipu;
+
+
+        public void VystrelVsechnySipy()
+        {
+            PocetSipu = PocetSipu - 10;
+            Console.WriteLine($"Počet šípu je {PocetSipu}.");
+        }
+    }
+}
---
+++ ukol4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ukol4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lucistnik archer = new Lucistnik();
+
+            archer.PocetSipu = 10;
+
+            archer.VystrelVsechnySipy();
+
+            
+            
+            
+        }
+    }
+}
